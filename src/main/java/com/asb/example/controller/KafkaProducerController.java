b5aa470a9e45ce1ef018be1a2a7489af385e3c3f
Konstantin <kss47242@gmail.com>
2021-05-04 15:37:36 +0300

    AddSomeEdits

--- src/main/java/com/asb/example/controller/KafkaProducerController.java
+++ src/main/java/com/asb/example/controller/KafkaProducerController.java
@@ -1,13 +1,9 @@
 package com.asb.example.controller;
 
-import java.util.concurrent.ExecutionException;
-
 import com.asb.example.entity.Book;
 import com.asb.example.service.KafkaProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaProducerController {
 
-	@Autowired
-	private  KafkaProducerService kafkaProducerService;
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
 
-	@PostMapping("/add-book")
-	public String postModelToKafka(@RequestBody Book book)
-			throws InterruptedException, ExecutionException {
-		return kafkaProducerService.postModelToKafka(book).get().getProducerRecord().value();
-	}
+    @PostMapping("/add-book")
+    public HttpStatus postModelToKafka(@RequestBody Book book) {
+        kafkaProducerService.postModelToKafka(book);
+        return HttpStatus.OK;
+    }
 }
